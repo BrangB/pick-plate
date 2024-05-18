60295d2745be265aaca38eb0fb528b0107a9f5f7
Brang <brangtsawmaung89@gmail.com>
2024-05-19 00:08:12 +0630

    some change

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js"
 import 'dotenv/config'
 
-const supabaseUrl = "https://bblniufqxpsusxqmiswt.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJibG5pdWZxeHBzdXN4cW1pc3d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwMDQ2ODUsImV4cCI6MjAzMTU4MDY4NX0.10pr8JIFK4Cu_fFgatUO1hucLIi4ZSHWqpb89_b62-Y"
+// const supabaseUrl = "https://bblniufqxpsusxqmiswt.supabase.co"
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJibG5pdWZxeHBzdXN4cW1pc3d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwMDQ2ODUsImV4cCI6MjAzMTU4MDY4NX0.10pr8JIFK4Cu_fFgatUO1hucLIi4ZSHWqpb89_b62-Y"
 
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseKey = process.env.SUPABASE_ANON_URL as string
 export const supabase = createClient(supabaseUrl, supabaseKey)
